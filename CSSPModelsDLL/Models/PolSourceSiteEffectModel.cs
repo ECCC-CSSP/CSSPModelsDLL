59da0b42c6ef2739c4eb25e8a787a02944fa6f83
ECCC-CSSP <charles.leblanc2@canada.ca>
2019-09-06 16:27:51 -0300

    Init

--- CSSPModelsDLL/Models/PolSourceSiteEffectModel.cs
+++ CSSPModelsDLL/Models/PolSourceSiteEffectModel.cs
@@ -14,8 +14,8 @@ namespace CSSPModelsDLL.Models
             PolSourceSiteEffectTermModelList = new List<PolSourceSiteEffectTermModel>();
         }
         public int PolSourceSiteEffectID { get; set; }
-        public int PolSourceSiteTVItemID { get; set; }
-        public int MWQMSiteOrInfrastructureTVItemID { get; set; }
+        public int PolSourceSiteOrInfrastructureTVItemID { get; set; }
+        public int MWQMSiteTVItemID { get; set; }
         public string PolSourceSiteEffectTermIDs { get; set; }
         public string Comments { get; set; }
         public Nullable<int> Site { get; set; }
